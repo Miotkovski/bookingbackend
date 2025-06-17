bf145a4cd56f9a37948149db4667b51cba9210d6
Dawid Miotk 77782 <miotkdawid@gmail.com>
2025-06-17 12:43:02 +0200

    Fix dotnev load order znowu xd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// server.js
-const { Pool } = require('pg');      
-require('dotenv').config();
+require('dotenv').config(); // <- najpierw ładujemy .env!
+const { Pool } = require('pg'); // <- dopiero potem korzystamy z env
+
 const express = require('express');
 const cors = require('cors');
 
@@ -10,7 +10,7 @@ const pool = new Pool({
 });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
